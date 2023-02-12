bf39a3a31619b4ff43de331f60e6a761f409afab
encoderpie <coderraenn@gmail.com>
2023-02-13 00:34:45 +0300

    Added Hamburger Menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaGithub, FaTerminal } from 'react-icons/fa'
+import { CgClose } from 'react-icons/cg'
 import { FiMenu } from 'react-icons/fi'
 
 const Navbar = () => {
+    const [hamburgerMenuOpen, setHamburgerMenuOpen] = useState(false)
     return (
         <>
             <div className='w-100 p-3 bg-gray-900'>
                 <div className='container flex flex-wrap font-medium justify-between items-center mx-auto'>
                     <div className='flex'>
-                        <FaTerminal size="29px"/>
+                        <FaTerminal size="29px" />
                         <p className='self-center text-xl font-semibold whitespace-nowrap ml-2 dark:text-white'>Encoderpie</p>
                     </div>
-                    <button className='block lg:hidden rounded-lg bg-slate-800 p-2 hover:bg-slate-800/50 transition-all'>
-                        <FiMenu />
+                    <button onClick={()=>setHamburgerMenuOpen(!hamburgerMenuOpen)} className='block lg:hidden rounded-lg bg-slate-800 p-2 hover:bg-slate-800/50 transition-all'>
+                        {hamburgerMenuOpen ? <CgClose /> : <FiMenu />}
                     </button>
                     <ul className="hidden lg:flex flex-col mt-4 bg-gray-50 rounded-lg md:flex-row md:space-x-6 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
                         <li>
                             <a className="pb-4 hover:pb-2 transition-all border-b-2 hover:border-blue-300 hover:border-x-0 hover:border-t-0"
-                            href="/#">
+                                href="/#">
                                 Home
                             </a>
                         </li>
                         <li>
                             <a className="hover:text-white pb-4 hover:pb-2 text-gray-400 transition-all border-b-2 border-gray-600 hover:border-blue-300 hover:border-x-0 hover:border-t-0"
-                            href="/#aboutme">
+                                href="/#aboutme">
                                 About Me
                             </a>
                         </li>
                         <li>
                             <a className="hover:text-white pb-4 hover:pb-2 text-gray-400 transition-all border-b-2 border-gray-600 hover:border-blue-300 hover:border-x-0 hover:border-t-0"
-                            href="/#skills">
+                                href="/#skills">
                                 Skills
                             </a>
                         </li>
                         <li>
                             <a className="hover:text-white pb-4 hover:pb-2 text-gray-400 transition-all border-b-2 border-gray-600 hover:border-blue-300 hover:border-x-0 hover:border-t-0"
-                            href="/#contact">
+                                href="/#contact">
                                 Contact
                             </a>
                         </li>
@@ -49,9 +51,33 @@ const Navbar = () => {
                                 hub
                             </div>
                         </div>
-                        <FaGithub size="23"/>
+                        <FaGithub size="23" />
                     </a>
                 </div>
+            </div>
+            <div className={`w-100 p-3 bg-slate-900 ${hamburgerMenuOpen ? 'block' : 'hidden'}`}>
+                <ul className='border rounded-lg border-slate-800 py-1 px-2'>
+                    <li>
+                        <a href="/#" className='hover:text-slate-200'>
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a className="text-gray-400 hover:text-slate-100" href="/#aboutme">
+                            About Me
+                        </a>
+                    </li>
+                    <li>
+                        <a className="text-gray-400 hover:text-slate-100" href="/#skills">
+                            Skills
+                        </a>
+                    </li>
+                    <li>
+                        <a className="text-gray-400 hover:text-slate-100" href="/#contact">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
             </div>
         </>
     )
